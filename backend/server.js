6cdace16bf6adb88ff80a3287daad1432aa07360
Destiny <110328571+Max637ctrl@users.noreply.github.com>
2025-01-30 18:32:08 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = 4000;
 
 // Middleware to parse JSON and handle CORS
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://destiny-tch.vercel.app' }))
 
 // Connect to the database
 connectDB();
